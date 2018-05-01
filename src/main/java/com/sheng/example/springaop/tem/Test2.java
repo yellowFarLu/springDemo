85058b5209c53d275f9370dcd8cf1dfc04fc7f53
Huangy <296947440@qq.com>
2018-05-01 22:36:36 +0800

    test2

---
+++ src/main/java/com/sheng/example/springaop/tem/Test2.java
@@ -0,0 +1,12 @@
+package com.sheng.example.springaop.tem;
+
+/**
+ * @author huangy on 2018/5/1
+ */
+public class Test2 {
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
